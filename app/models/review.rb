4bd32f379f0c45de72468679ddda265466f8ea04
David Neuman <david.m.neuman@gmail.com>
2019-04-19 20:13:50 +0000

    Added validations to the review model.

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,7 +6,7 @@ class Review < ActiveRecord::Base
   validates :product, presence: true
   validates :user, presence: true
   validates :description, presence: true
-  validates :rating, presence :true, numericality: {
+  validates :rating, presence: true, numericality: {
     only_integer: true,
     greater_than: 0,
     less_than: 6

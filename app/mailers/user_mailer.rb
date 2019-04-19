0fc4ce94d2da2408eea304ae35e92ebec0cd878e
David Neuman <david.m.neuman@gmail.com>
2019-04-19 19:29:59 +0000

    Added instance variable line_items to allow for data to be passed to the mailer view.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ApplicationMailer
   def confirmation_email(email, order)
     @email = email
     @order = order
+    @line_items = order.line_items
     mail(to: email, subject: "Your Jungle Order Confirmation #: #{order.id}")
   end
 

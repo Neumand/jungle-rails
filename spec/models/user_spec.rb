f9b73c788e4c069bce3ace5acb57d3b25dec810b
David Neuman <david.m.neuman@gmail.com>
2019-04-22 16:51:16 +0000

    Initial setup for test examples.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:first_name) {'John'}
+  let(:last_name) {'Smith'}
+  let(:email) {'@jsmith@example.com'}
+  let(:password) {'p@ssw0rd'}
+  let(:password_confirmation) {'p@ssw0rd'}
+  let(:user) {User.new first_name: first_name, last_name: last_name, email: email, password: password, password_confirmation: password_confirmation}
+
+  
 end

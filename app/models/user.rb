2b21ca32713725afdc708f740f241a531560c964
David Neuman <david.m.neuman@gmail.com>
2019-04-22 16:47:00 +0000

    Additional vaidation for password length.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { in: 6..20 }
 
 end

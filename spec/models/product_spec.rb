cd562219b63a2a57e60d202591d3d506a5609ba8
David Neuman <david.m.neuman@gmail.com>
2019-04-22 16:30:22 +0000

    Added test examples for name, price, quantity and category presence validations from the Product model. These test the error display messages.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,11 +9,45 @@ RSpec.describe Product, type: :model do
 
   describe 'Validations' do
 
-    context "when all required fields are provided" do
-      it 'is valid with valid attributes' do
-        expect(product).to be_valid
+    context 'when all required fields are provided' do
+      it 'is not reporting any validation errors' do
+        product.valid?
+        expect(product.errors.full_messages).to be_empty
       end
     end
+
+    context 'when name field is not provided' do
+      let(:name) {nil}
+      it 'should show an error validation message' do
+        product.valid?
+        expect(product.errors.full_messages).to include("Name can't be blank")
+      end
+    end
+
+    context 'when price field is not provided' do
+      let(:price) {nil}
+      it 'should show an error validation message' do
+        product.valid?
+        expect(product.errors.full_messages).to include("Price can't be blank")
+      end
+    end
+
+    context 'when quantity field is not provided' do
+      let(:quantity) {nil}
+      it 'should show an error validation message' do
+        product.valid?
+        expect(product.errors.full_messages).to include("Quantity can't be blank")
+      end
+    end
+
+    context 'when category field is not provided' do
+      let (:category) {nil}
+      it 'should show an error validation message' do
+        product.valid?
+        expect(product.errors.full_messages).to include("Category can't be blank")
+      end
+    end
+
   end
     
 

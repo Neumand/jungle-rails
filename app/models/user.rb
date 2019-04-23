6c837a22f784b38b1b345e31873511283a2d7be2
David Neuman <david.m.neuman@gmail.com>
2019-04-23 21:15:01 +0000

    Edit password validation to fix bug which prevents users from signing up successfully. Remove presence: true from the validation to accomplish this.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }, confirmation: true
+  validates :password, length: { minimum: 6 }
 
   def self.authenticate_with_credentials(email, password)
     user = User.find_by_email(email)
